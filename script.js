89a379b0f85b8780b98aa9105a85d5f08c861c2f
Amrut Sagar <amrutsagar06@gmail.com>
2022-06-16 14:03:08 +0530

    functions for moving keys

--- script.js
+++ script.js
@@ -58,6 +58,124 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  // swipe right
+
+  function moveRight() {
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i + 1].innerHTML;
+        let totalThree = squares[i + 2].innerHTML;
+        let totalFour = squares[i + 3].innerHTML;
+
+        // now these numbers are in the form of stirngs. we need to change it into intergers
+
+        let row = [
+          parseInt(totalOne),
+          parseInt(totalTwo),
+          parseInt(totalThree),
+          parseInt(totalFour),
+        ]; // parseInt() : parses a string and returns an integer.
+        let filteredRow = row.filter((num) => num); // filter() : creates an array filled with all array elements that pass a test (provided as a function).
+        // it filter out all the number from our rows
+
+        let missing = 4 - filteredRow.length;
+        let zeros = Array(missing).fill(0); // fill() :  fills the specified elements in an array with a static value.
+        let newRow = zeros.concat(filteredRow); // concat() : used to join two or more strings.
+
+        squares[i].innerHTML = newRow[0];
+        squares[i + 1].innerHTML = newRow[1];
+        squares[i + 2].innerHTML = newRow[2];
+        squares[i + 3].innerHTML = newRow[3];
+      }
+    }
+  }
+
+  // swipe left
+
+  function moveLeft() {
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML;
+        let totalTwo = squares[i + 1].innerHTML;
+        let totalThree = squares[i + 2].innerHTML;
+        let totalFour = squares[i + 3].innerHTML;
+        let row = [
+          parseInt(totalOne),
+          parseInt(totalTwo),
+          parseInt(totalThree),
+          parseInt(totalFour),
+        ];
+
+        let filteredRow = row.filter((num) => num);
+
+        let missing = 4 - filteredRow.length;
+        let zeros = Array(missing).fill(0);
+
+        let newRow = filteredRow.concat(zeros);
+
+        squares[i].innerHTML = newRow[0];
+        squares[i + 1].innerHTML = newRow[1];
+        squares[i + 2].innerHTML = newRow[2];
+        squares[i + 3].innerHTML = newRow[3];
+      }
+    }
+  }
+
+  //swipe DOwn
+
+  function moveDown() {
+    for (let i = 0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML;
+      let totalTwo = squares[i + width].innerHTML;
+      let totalThree = squares[i + width * 2].innerHTML;
+      let totalFour = squares[i + width * 3].innerHTML;
+
+      let column = [
+        parseInt(totalOne),
+        parseInt(totalTwo),
+        parseInt(totalThree),
+        parseInt(totalFour),
+      ];
+
+      let filteredCol = column.filter((num) => num);
+      let missing = 4 - filteredCol.length;
+      let zeros = Array(missing).fill(0);
+      let newColumn = zeros.concat(filteredCol);
+
+      squares[i].innerHTML = newColumn[0];
+      squares[i + width].innerHTML = newColumn[1];
+      squares[i + width * 2].innerHTML = newColumn[2];
+      squares[i + width * 3].innerHTML = newColumn[3];
+    }
+  }
+  // swipe Up
+
+  function moveUp() {
+    for (let i = 0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML;
+      let totalTwo = squares[i + width].innerHTML;
+      let totalThree = squares[i + width * 2].innerHTML;
+      let totalFour = squares[i + width * 3].innerHTML;
+      let column = [
+        parseInt(totalOne),
+        parseInt(totalTwo),
+        parseInt(totalThree),
+        parseInt(totalFour),
+      ];
+
+      let filteredCol = column.filter((num) => num);
+      let missing = 4 - filteredCol.length;
+      let zeros = Array(missing).fill(0);
+      let newColumn = filteredCol.concat(zeros);
+
+      squares[i].innerHTML = newColumn[0];
+      squares[i + width].innerHTML = newColumn[1];
+      squares[i + width * 2].innerHTML = newColumn[2];
+      squares[i + width * 3].innerHTML = newColumn[3];
+    }
+  }
+
   // add all the digits in a row as per the left or right movement
 
   function combineRow() {
